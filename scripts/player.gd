3a828fff649f3d8732ac8a85a9b50794b6170b42
tattikhorjafsa-netizen <mehmoodashrafhafsa639@gmail.com>
2026-09-19 19:00:31 +0500

    JAFSA'S GAME :3
    a cutesy character jumping around in a cutesy scene with cutesy sound effects and cutesy animations and then falling into an eternal abyss :3

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,18 +1,29 @@
 extends CharacterBody2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var jump_sound: AudioStreamPlayer2D = $"jump-sound"
 
 
-const SPEED = 900.0
-const JUMP_VELOCITY = -900.0
+const SPEED = 300.0
+const JUMP_VELOCITY = -850.0
 
 
 func _physics_process(delta: float) -> void:
+	
+	# Add animation
+	if velocity.x > 1 or velocity.x < -1:
+		animated_sprite_2d.animation = "running"
+	else:
+		animated_sprite_2d.animation = "idle"
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		animated_sprite_2d.animation = "jumping"
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		jump_sound.play()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -23,3 +34,8 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	
+	if direction == 1.0:
+		animated_sprite_2d.flip_h = false
+	elif direction == -1.0:
+		animated_sprite_2d.flip_h = true
